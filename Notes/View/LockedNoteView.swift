9eeae9c1d4442179ade556dee41bd71f5736dee7
Jem Alvarez <jemalvarez22@gmail.com>
2021-07-01 01:00:55 -0400

    Note authentication with biometric/passcode

--- Notes/View/LockedNoteView.swift
+++ Notes/View/LockedNoteView.swift
@@ -1,6 +1,7 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 // locked note view popup
 struct LockedNoteView: View {
@@ -20,7 +21,7 @@ struct LockedNoteView: View {
                 
                 // button
                 Button(action: {
-                    locked = false
+                    authenticate()
                 }) {
                     Text("UNLOCK!")
                         .padding(10)
@@ -32,6 +33,35 @@ struct LockedNoteView: View {
             .padding(30)
             .background(Color(.systemGray5).opacity(0.5))
             .cornerRadius(15)
+        }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                DispatchQueue.main.async {
+                    if success {
+                        // authenticated successfully
+                        withAnimation {
+                            locked = false
+                        }
+                    } else {
+                        // there was a problem
+                        print("ERROR")
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+            print("NO BIOMETRICS")
         }
     }
 }
